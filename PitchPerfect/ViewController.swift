fd59968a39a5f67898117b30ec9f8ce1f4049982
willbrom <alisajid552@yahoo.com>
2018-04-23 10:09:51 +0300

    Add 'Yo mama' joke..

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var defaultTextField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,8 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onClickRecord(_ sender: Any) {
+        defaultTextField.text = "Yo mama so fat, when she walks past the TV, you miss 3 episodes of the show!"
+    }
 }
 
